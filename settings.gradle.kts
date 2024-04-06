138a472402d2020f044f0f5d74d997110edef387
Phyrone <phyrone@phyrone.de>
2024-04-06 11:14:12 +0200

    fix typo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-rootProject.name = "project-kiwi-c2"
+rootProject.name = "project-kiwi"
 
 include(
     ":gateway",

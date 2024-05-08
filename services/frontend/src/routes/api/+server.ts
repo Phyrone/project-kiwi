fe605615c4d449d43a5bdf58cbe44fbce3efa7cb
Phyrone <phyrone@phyrone.de>
2024-05-08 09:32:30 +0200

    🚧 Snapshot and Rebase from Online

---
+++ services/frontend/src/routes/api/+server.ts
@@ -0,0 +1,7 @@
+import { redirect } from '@sveltejs/kit';
+
+export const trailingSlash = "ignore";
+
+export const GET = async () => {
+	redirect(302, '/api/v1/');
+};
---
+++ services/frontend/src/types/common.ts
@@ -0,0 +1,3 @@
+
+
+export type FetchFunction = typeof fetch;
